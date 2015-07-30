941bedbcb7fc55903eae00c3b0eafa5544459f7d
KS Chan <mrkschan@gmail.com>
2015-07-30 23:58:07 +0800

    Start basic proxy

--- cuttle/cuttle.go
+++ cuttle/cuttle.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"net/http"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/elazarl/goproxy"
+)
+
 func main() {
+	log.SetFormatter(&log.TextFormatter{})
+
+	proxy := goproxy.NewProxyHttpServer()
+	proxy.Verbose = true
+	log.Fatal(http.ListenAndServe(":8123", proxy))
 }
