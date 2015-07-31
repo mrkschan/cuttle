1a7e60e04f06bf9a8fc5914e2b3a5eded775ba66
KS Chan <mrkschan@gmail.com>
2015-07-31 16:26:42 +0800

    Introduced config file cuttle.conf

--- cuttle/cuttle.go
+++ cuttle/cuttle.go
@@ -4,13 +4,22 @@ import (
 	"net/http"
 
 	log "github.com/Sirupsen/logrus"
+	"github.com/cloudflare/conf"
 	"github.com/elazarl/goproxy"
 )
 
 func main() {
 	log.SetFormatter(&log.TextFormatter{})
 
+	config, err := conf.ReadConfigFile("cuttle.conf")
+	if err != nil {
+		log.Error("Failed to load config from 'cuttle.conf'.")
+		log.Fatal(err)
+	}
+	addr := config.GetString("addr", ":8123")
+	verbose := config.GetUint("verbose", 0)
+
 	proxy := goproxy.NewProxyHttpServer()
-	proxy.Verbose = true
-	log.Fatal(http.ListenAndServe(":8123", proxy))
+	proxy.Verbose = verbose == 1
+	log.Fatal(http.ListenAndServe(addr, proxy))
 }
